ddf1e4025451c343692a12eae93e298e9d22b484
IamPedrin <pedrin.luiz22@gmail.com>
2025-07-04 16:21:06 -0300

    update deploy url

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://tracker-api-bh00.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
